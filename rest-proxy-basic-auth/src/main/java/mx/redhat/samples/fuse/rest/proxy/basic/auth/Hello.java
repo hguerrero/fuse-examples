3675496a36770bd6c98ad8dd8e157b3a501138f6
hguerrero <hguerrer@localhost>
2016-07-04 20:31:55 -0500

    initial commit

---
+++ rest-proxy-basic-auth/src/main/java/mx/redhat/samples/fuse/rest/proxy/basic/auth/Hello.java
@@ -0,0 +1,10 @@
+package mx.redhat.samples.fuse.rest.proxy.basic.auth;
+
+/**
+ * An interface for implementing Hello services.
+ */
+public interface Hello {
+
+    String hello();
+	
+}
---
+++ rest-proxy-basic-auth/src/main/java/mx/redhat/samples/fuse/rest/proxy/basic/auth/HelloBean.java
@@ -0,0 +1,25 @@
+package mx.redhat.samples.fuse.rest.proxy.basic.auth;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * A bean which we use in the route
+ */
+public class HelloBean implements Hello {
+
+    private String say = "Hello World";
+
+    public String hello() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return say + " at " + sdf.format(new Date());
+    }
+
+    public String getSay() {
+        return say;
+    }
+
+    public void setSay(String say) {
+        this.say = say;
+    }
+}
